e662b72c08d4ffcdfbf5c23d40595f94ebd30a9e
Marko Rajic <rajicm@hotmail.com>
2025-10-28 19:05:18 +0100

    Added netlify domen

--- koneti-backend/server.js
+++ koneti-backend/server.js
@@ -31,7 +31,8 @@ app.use(compression());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  'https://koneti-cafe-frontend.onrender.com'
+  'https://koneti-cafe-frontend.onrender.com',
+  'https://koneti.netlify.app/'
 ];
 
 app.use(cors({
